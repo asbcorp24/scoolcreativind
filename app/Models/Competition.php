dd5eb0659f89889a40b63e935fadb130c89b2483
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-24 22:35:44 +0300

    Add learning and achievements models

---
+++ app/Models/Competition.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class Competition extends Model {
+ protected $fillable=['title','organizer','starts_on','ends_on','location','description','url','cover','is_published'];
+ protected $casts=['starts_on'=>'date','ends_on'=>'date','is_published'=>'boolean'];
+ public function achievements(){return $this->hasMany(Achievement::class);}
+}
