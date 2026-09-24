7a197528f76fcb804ec1f667e358496216a6406d
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-25 00:06:27 +0300

    Link competitions to participant registrations

--- app/Models/Competition.php
+++ app/Models/Competition.php
@@ -5,4 +5,5 @@ class Competition extends Model {
  protected $fillable=['title','organizer','starts_on','ends_on','location','description','url','cover','is_published'];
  protected $casts=['starts_on'=>'date','ends_on'=>'date','is_published'=>'boolean'];
  public function achievements(){return $this->hasMany(Achievement::class);}
+ public function registrations(){return $this->hasMany(CompetitionRegistration::class);}
 }
