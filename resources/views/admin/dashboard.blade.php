272983bacf4d8830aff7aa99923ddcb003264616
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-25 23:04:08 +0300

    Add music section to admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -20,6 +20,7 @@
 @if(auth()->user()->canAdminSection('documents'))<a href="{{ route('admin.documents') }}" class="btn btn-ghost">Документы</a>@endif
 @if(auth()->user()->canAdminSection('questions'))<a href="{{ route('admin.questions') }}" class="btn btn-ghost">Вопросы</a>@endif
 @if(auth()->user()->canAdminSection('contacts'))<a href="{{ route('admin.contacts') }}" class="btn btn-ghost">Контакты</a>@endif
+@if(auth()->user()->canAdminSection('music'))<a href="{{ route('admin.music') }}" class="btn btn-ghost">Музыка</a>@endif
 @if(auth()->user()->is_admin)<a href="{{ route('admin.access-admins') }}" class="btn btn-ghost">Администраторы</a>@endif
 </div></div></div></section>
 <section class="pb-5"><div class="container">
